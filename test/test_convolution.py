86fee4baf11a36483ecd141d60f3445d9ebafa17
pradal <christophe.pradal@cirad.fr>
2025-11-18 17:38:41 +0100

    Add test

--- test/test_convolution.py
+++ test/test_convolution.py
@@ -6,96 +6,87 @@
 
 __version__ = "$Id$"
 
+from .tools import interface, runTestClass
+
 from openalea.stat_tool import Convolution, Estimate
 from openalea.stat_tool.data_transform import ExtractDistribution
 from openalea.stat_tool.distribution import Binomial, NegativeBinomial
 
-from .tools import interface, runTestClass
 
+import pytest
+@pytest.fixture
+def data():
+    d1 = Binomial(0, 10, 0.5)
+    d2 = NegativeBinomial(0, 1, 0.1)
+    conv = Convolution(d1, d2)
+    return conv
 
-class Test(interface):
-    """a simple unittest class
+@pytest.fixture
+def myi(data):
+    return interface(data, "data/convolution1.conv", Convolution)
 
-    See tools to get documentation of the following methods
-    """
+def test_constructor_from_file(myi):
+    myi.constructor_from_file()
 
-    def __init__(self):
-        interface.__init__(
-            self, self.build_data(), "data/convolution1.conv", Convolution
-        )
+def test_constructor_from_file_failure(myi):
+    myi.constructor_from_file_failure()
 
-    def build_data(self):
-        d1 = Binomial(0, 10, 0.5)
-        d2 = NegativeBinomial(0, 1, 0.1)
-        conv = Convolution(d1, d2, d1, d2)
-        conv = Convolution(d1, d2)
-        return conv
+def test_print(myi):
+    myi.print_data()
 
-    def test_constructor_from_file(self):
-        self.constructor_from_file()
+def test_display(myi):
+    myi.display()
+    myi.display_versus_ascii_write()
+    myi.display_versus_str()
 
-    def test_constructor_from_file_failure(self):
-        self.constructor_from_file_failure()
+def test_len(myi):
+    c = myi.data
+    assert len(c) == 2
+    assert len(c) == c.nb_distribution()
 
-    def test_print(self):
-        self.print_data()
+def test_plot(myi):
+    d = myi.data
+    d.plot()
 
-    def test_display(self):
-        self.display()
-        self.display_versus_ascii_write()
-        self.display_versus_str()
+def test_save(myi):
+    myi.save()
 
-    def test_len(self):
-        c = self.data
-        assert len(c) == 2
-        assert len(c) == c.nb_distribution()
+def test_plot_write(myi):
+    myi.plot_write()
 
-    def test_plot(self):
-        d = self.data
-        d.plot()
+def test_file_ascii_write(myi):
+    myi.file_ascii_write()
 
-    def test_save(self):
-        self.save()
+def test_spreadsheet_write(myi):
+    myi.spreadsheet_write()
 
-    def test_plot_write(self):
-        self.plot_write()
+def test_simulate(myi):
+    myi.simulate()
 
-    def test_file_ascii_write(self):
-        self.file_ascii_write()
+def test_extract(data):
+    """run and test the extract methods"""
 
-    def test_spreadsheet_write(self):
-        self.spreadsheet_write()
+    m = data
+    assert m.extract_convolution() == ExtractDistribution(m, "Convolution")
+    assert m.extract(1) == Binomial(0, 10, 0.5)
+    assert m.extract(2) == NegativeBinomial(0, 1, 0.1)
+    assert ExtractDistribution(m, "Elementary", 1) == Binomial(0, 10, 0.5)
+    assert ExtractDistribution(m, "Elementary", 2) == NegativeBinomial(0, 1, 0.1)
 
-    def test_simulate(self):
-        self.simulate()
+def test_extract_data(data):
+    """run and test the extract_data methods"""
 
-    def test_extract(self):
-        """run and test the extract methods"""
+    m = data
+    s = m.simulate(1000)
+    e = s.estimate_convolution(Binomial(0, 10, 0.5), Estimator="Parametric")
+    d = e.extract_data()
+    assert d
+    eprime = Estimate(s, "CONVOLUTION", Binomial(0, 10, 0.5))
 
-        m = self.data
-        assert m.extract_convolution() == ExtractDistribution(m, "Convolution")
-        assert m.extract(1) == Binomial(0, 10, 0.5)
-        assert m.extract(2) == NegativeBinomial(0, 1, 0.1)
-        assert ExtractDistribution(m, "Elementary", 1) == Binomial(0, 10, 0.5)
-        assert ExtractDistribution(m, "Elementary", 2) == NegativeBinomial(0, 1, 0.1)
+    # todo: find robust assert ?
+    print(eprime.get_mean)
 
-    def test_extract_data(self):
-        """run and test the extract_data methods"""
+def test_truncate(data):
+    s = data
+    _res = s.truncate(4)
 
-        m = self.data
-        s = m.simulate(1000)
-        e = s.estimate_convolution(Binomial(0, 10, 0.5), Estimator="Parametric")
-        d = e.extract_data()
-        assert d
-        eprime = Estimate(s, "CONVOLUTION", Binomial(0, 10, 0.5))
-
-        # todo: find robust assert ?
-        print(eprime.get_mean)
-
-    def test_truncate(self):
-        s = self.data
-        _res = s.truncate(4)
-
-
-if __name__ == "__main__":
-    runTestClass(Test())
